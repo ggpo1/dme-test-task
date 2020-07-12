040a944f5031112f263b4f75aab3a94b2e7914f0
Vladimir Arkhangelskii <ggpo1@192.168.1.5>
2020-07-12 13:03:36 +0300

    project structure

--- js/index.js
+++ js/index.js
@@ -15,15 +15,28 @@ const store = createStore(reducer, initialState);
 
 
 $(document).ready(() => {
-    store.subscribe(() => console.log(store.getState()));
+    store.subscribe(() => {
+        console.log('update state');
+        console.log(store.getState());
+        console.log(Object.keys(store.getState().data));
+        console.log('____________________');
+    });
 });
 
+// setInterval(
+//     () => {
+//         $.getJSON(
+//             'https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=56.84,55.27,33.48,41.48',
+//             (data) => store.dispatch(fillAction(data))
+//         )
+//     },
+//     3000
+// )
 
 $.getJSON(
     'https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=56.84,55.27,33.48,41.48',
     (data) => store.dispatch(fillAction(data))
 )
-
 
 
 // console.log(store.getState());
